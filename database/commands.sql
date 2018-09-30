83275ed4b9a3c1767380f1657b0a30bf46fd2fbf
Stiven <st.stc200@gmail.com>
2018-10-01 00:08:32 +0200

    Update commands.sql

--- database/commands.sql
+++ database/commands.sql
@@ -1,40 +1,40 @@
-SELECT * FROM genin;
+SELECT * FROM nindza;
 CREATE VIEW pogled1 AS
-SELECT  ImeNinje,Cakra FROM jounin
+SELECT  ImeNinje,Cakra FROM nindza
 WHERE ImeNinje ="Naruto";
 CREATE VIEW broj AS 
-SELECT Cakra,Ninjutsu,Stamina FROM kage
+SELECT Cakra,Ninjutsu,Stamina FROM nindza
 WHERE Cakra >12;
 CREATE PROCEDURE selectNinja  ()
-SELECT * FROM kage
+SELECT * FROM nindza
 WHERE ImeNinje ='Sasuke';
 CREATE PROCEDURE `FirstProcedure` ( ime varchar(42))
 BEGIN
-SELECT ImeNinje  FROM summon
+SELECT ImeNinje  FROM nindza
 where ime=ImeNinje;
 END
 
 CREATE PROCEDURE `FirstProcedure` ( ime varchar(42))
 
-SELECT ImeNinje  FROM summon
+SELECT ImeNinje  FROM nindza
 where ime=ImeNinje
 
 START TRANSACTION;
-DELETE FROM summon  WHERE  idSummon=2;
+DELETE FROM nindza  WHERE  idSummon=2;
 COMMIT;
 
 delimiter //
- CREATE TRIGGER trigger_genin BEFORE UPDATE ON genin
+ CREATE TRIGGER trigger_genin BEFORE UPDATE ON nindza
        FOR EACH ROW
        BEGIN
-           IF NEW.Cakra < (SELECT MIN(Cakra) FROM genin WHERE idGenin=NEW.idGenin  )  THEN
+           IF NEW.Cakra < (SELECT MIN(Cakra) FROM nindza WHERE idNindza=NEW.idNindza  )  THEN
                SIGNAL SQLSTATE "50001" SET MESSAGE_TEXT = 'Kada unosite novog nindzu cakra mora da ima minimalnu vrednost 1';
            
            END IF;
        END;//
  delimiter ;
  delimiter //
- CREATE TRIGGER trigger_name BEFORE UPDATE ON genin
+ CREATE TRIGGER trigger_name BEFORE UPDATE ON nindza
        FOR EACH ROW
        BEGIN
            IF char_length(new.`ImeNinje`) <2  THEN
